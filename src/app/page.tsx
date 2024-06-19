1feb97270cc99662db6377259ed5d7cbcef929a7
Alix F. Leon <alixlm19@hotmail.com>
2024-06-18 23:48:16 -0400

    Changed heading msg in home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,16 +10,17 @@ export default function Home() {
   return (
     <Container>
       <span className="text-4xl">👋</span>
-      <Heading className="font-black">Hello there! I&apos;m John</Heading>
+      <Heading className="font-black">Hello there! I&apos;m Alix</Heading>
       <Paragraph className="max-w-xl mt-4">
-        I&apos;m a full-stack developer that loves{" "}
-        <Highlight>building products</Highlight> and web apps that can impact
-        millions of lives
+        I&apos;m a data scientist / software engineer that loves{" "}
+        <Highlight>building products with data</Highlight> and using science-based approaches
+        to find the hidden patterns, generating actionable insights
+        that can impact millions of lives
       </Paragraph>
       <Paragraph className="max-w-xl mt-4">
-        I&apos;m a senior software engineer with{" "}
-        <Highlight>7 years of experience</Highlight> building scalable web apps
-        that are performance optimized and good looking.
+        I&apos;m have more than{" "}
+        <Highlight>3 years of experience</Highlight> building scalable and robust products,
+        creating groundbreaking analises, and delivering powerfull predictions and insights.  
       </Paragraph>
       <Heading
         as="h2"
