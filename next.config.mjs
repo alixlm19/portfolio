5d1dfaef726ca11a2078a3f9068c0261e7818b94
Alix F. Leon <alixlm19@hotmail.com>
2024-06-19 01:26:54 -0400

    Changed stack icons and profile img

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ import rehypePrism from "@mapbox/rehype-prism";
 
 const nextConfig = {
   images: {
-    domains: ["images.unsplash.com", "res.cloudinary.com"],
+    domains: ["images.unsplash.com", "www.github.com", "res.cloudinary.com"],
   },
   experimental: {
     mdxRs: true,
