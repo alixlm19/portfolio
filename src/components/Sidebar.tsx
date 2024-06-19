22187cdd217b08ec770bd715f60eb2f916ead8ac
Alix Leon <alixlm19@hotmail.com>
2024-06-19 01:26:54 -0400

    Changed stack icons and profile img

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -109,15 +109,15 @@ const SidebarHeader = () => {
   return (
     <div className="flex space-x-2">
       <Image
-        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1760&q=80"
+        src="https://www.github.com/alixlm19.png"
         alt="Avatar"
         height="40"
         width="40"
         className="object-cover object-top rounded-full flex-shrink-0"
       />
       <div className="flex text-sm flex-col">
-        <p className="font-bold text-primary">John Doe</p>
-        <p className="font-light text-secondary">Developer</p>
+        <p className="font-bold text-primary">Alix Leon</p>
+        <p className="font-light text-secondary">Data Wizard</p>
       </div>
     </div>
   );
