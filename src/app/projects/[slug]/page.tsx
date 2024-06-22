47a0ec63d2f7938f6ca7571359a1ad15fa05d773
Alix Leon <alixlm19@hotmail.com>
2024-06-22 01:10:47 -0400

    Started Projects page modification

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -27,9 +27,9 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
     };
   } else {
     return {
-      title: "Projects | John Doe",
+      title: "Projects | Alix Leon",
       description:
-        "John Doe is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
+        "Alix leon is a developer, writer and speaker. He is a digital nomad and travels around the world while working remotely.",
     };
   }
 }
