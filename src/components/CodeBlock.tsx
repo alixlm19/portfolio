1c0585cbf9b5e8139815d9e3a7ec1e7ec471eff5
Alix Leon <alixlm19@hotmail.com>
2024-07-14 00:14:26 -0400

    updated transformer project page

--- src/components/CodeBlock.tsx
+++ src/components/CodeBlock.tsx
@@ -1,22 +1,28 @@
-import { bundledLanguages, createHighlighter } from 'shiki/bundle/web';
+import { bundledLanguages, createHighlighter, Highlighter } from 'shiki/bundle/web';
+import { useMemo } from 'react';
 
-const codeToHTML = async (
-    { code, lang }: { code: string, lang: string }
-) => {
-    const highlighter = await createHighlighter({
+const langs = [...Object.keys(bundledLanguages)]
+let highlighter: Highlighter | undefined = undefined;
+(async () => {
+    highlighter = await createHighlighter({
         themes: ['github-light', 'github-dark'],
-        langs: [
-            ...Object.keys(bundledLanguages),
-        ],
-    });
+        langs: langs
+    })
+})();
 
-    const html = highlighter.codeToHtml(code, {
-        lang: lang,
-        themes: {
-            dark: 'github-dark',
-            light: 'github-light',
-        },
-    });
+const codeToHTML = (
+    { code, lang = "python" }: { code: string, lang: string }
+) => {
+
+    const html = highlighter ?
+        highlighter.codeToHtml(code, {
+            lang: lang,
+            themes: {
+                dark: 'github-dark',
+                light: 'github-light',
+            },
+        }) :
+        `<p>${code}</p>`;
 
     return <div dangerouslySetInnerHTML={{ __html: html }} />;
 };
