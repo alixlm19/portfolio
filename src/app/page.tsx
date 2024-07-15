f18d6ee00669acb9d08afa6da560fcfbe8c53eee
Alix F. Leon <alixlm19@hotmail.com>
2024-07-14 23:40:17 -0400

    Added analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,6 @@ import { Highlight } from "@/components/Highlight";
 import { Paragraph } from "@/components/Paragraph";
 import { Products } from "@/components/Products";
 import { TechStack } from "@/components/TechStack";
-import Image from "next/image";
 
 export default function Home() {
   return (
